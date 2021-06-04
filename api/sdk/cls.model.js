3bd1e884f6ba3792515fa4b69db36defa4c01108
FabriandiRezkihasto <84980224+FabriandiRezkihasto@users.noreply.github.com>
2021-06-05 00:37:23 +0700

    Update cls.model.js

--- api/sdk/cls.model.js
+++ api/sdk/cls.model.js
@@ -37,7 +37,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/muzahdi/jst-muzahdi/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/FabriandiRezkihasto/bot-jst/main/api/sdk/cls.model.js';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
